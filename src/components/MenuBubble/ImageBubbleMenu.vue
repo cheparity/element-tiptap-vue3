8ef2c6e718eddd71bef5a6d11eecb801e347725e
Niyuta <cheparity@gmail.com>
2024-07-17 01:41:44 +0800

    feat: 完成了Image Ai的样式

--- src/components/MenuBubble/ImageBubbleMenu.vue
+++ src/components/MenuBubble/ImageBubbleMenu.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="image-bubble-menu">
-        <image-display-command-button :node="node" :update-attrs="updateAttrs" />
-
         <image-ai-button :node="node" :update-attrs="updateAttrs" />
-
+        <image-display-command-button :node="node" :update-attrs="updateAttrs" />
         <edit-image-command-button :node="node" :update-attrs="updateAttrs" />
         <remove-image-command-button :editor="editor" />
     </div>
